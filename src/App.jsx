fb8ffa2636d615169323e9768a27855eed1d44d5
JonathanLeon2610 <jonathan2610leon@gmail.com>
2023-08-15 10:52:54 -0700

    Subir el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AgentsCarousel from './Components/AgentsCarousel'
+import MapCarousel from './Components/MapsCarousel'
+import NavBar from './Components/NavBar'
+import WeaponSlider from './Components/WeaponsSlider'
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <AgentsCarousel/>
+      <MapCarousel/>
+      <WeaponSlider/>
+    </>
+  )
+}
+
+export default App
